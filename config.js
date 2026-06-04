311b9884300e1c106221da8cbf020083e6b53886
Gayath Chandira <gayath.chandira94@gmail.com>
2026-06-04 22:33:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   // Paste your published Google Apps Script Web App Deployment URL below:
-  SCRIPT_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyH6xCmfpr0QYK8dmGg6OCxv8x8XfNIsqHJKtj-zITg0jzpPqMzRaIve2vI7sWuKnmCGA/exec",
   
   // App Personalization Details
   APP_TITLE: "💰 Expense Tracker",
